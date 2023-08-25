e92f3df176766ad2ceaccacfca7a7950d448ca8b
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-08-25 12:57:15 +0000

    Changed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Destroying seeds!'
+Movie.destroy_all
 
 puts 'Seeding movies...'
 Movie.create!(title: 'Wonder Woman 1984',
