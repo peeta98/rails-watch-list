5288a28c55b9ebbffe4610e598a11cd2fa67dda4
Pedro Moreira <pedromnmoreira98@gmail.com>
2023-08-25 12:59:52 +0000

    Update Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts 'Destroying seeds!'
+Bookmark.destroy_all
+List.destroy_all
 Movie.destroy_all
 
 puts 'Seeding movies...'
